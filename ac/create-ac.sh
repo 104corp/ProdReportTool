6300006530d8f7469f34cc168b4433c3cd3f7584
wow73611 <wow73611@gmail.com>
2025-03-31 14:39:43 +0800

    update ac report

--- ac/create-ac.sh
+++ ac/create-ac.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 apps=(
-  "app=ac-api ns=p-ac-ac-api domain=acapi.104dc.com"
-  "app=bsignin ns=p-ac-bsignin domain=api.bsignin.104.com.tw"
-  "app=b-hydra ns=p-ac-b-hydra domain=boidc.104.com.tw"
-  "app=queue ns=p-ac-queue domain=unknown"
-  "app=schedule ns=p-ac-schedule domain=unknown"
+  "app=ac-api ns=p-ac-ac-api domain=acapi.104dc.com region=idc,aws"
+  "app=bsignin ns=p-ac-bsignin domain=api.bsignin.104.com.tw region=idc,aws"
+  "app=b-hydra ns=p-ac-b-hydra domain=boidc.104.com.tw region=idc,aws"
+#  "app=queue ns=p-ac-queue domain=unknown"
+#  "app=schedule ns=p-ac-schedule domain=unknown"
 )
 
-title="應用程式12月運行評估報告"
-create_time=$(date --iso-8601=seconds)
+title="應用程式 3月運行評估報告"
+## For Linux
+# create_time=$(date --iso-8601=seconds)
+## For MacOS
+create_time=$(date -u +'%Y-%m-%dT%H:%M:%S%z')
 export title
 export create_time
 
@@ -22,56 +25,90 @@ for item in "${apps[@]}"; do
     value=${pair#*=}
 
     case $key in
+      "app") export app=$value ;;
       "domain") export domain=$value ;;
       "ns") export ns=$value ;;
-      *) export app=$value ;;
+      "region") export region=$value ;;
+      *) export default=$value ;;
     esac
   done
 
+  IFS=',' read -ra reg_list <<< "${region}"
+
   echo -e "\n## 應用名稱: $app"
-  cpu_output=""
-  export cpu_output=$(curl -s "http://prom.apps.k8s.104dc.com/api/v1/query?query=sum(node_namespace_pod_container%3Acontainer_cpu_usage_seconds_total%3Asum_irate%7Bnamespace%3D%22p-ac-$app%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-prod-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)%20%2F%20sum(kube_pod_container_resource_limits%7Bjob%3D%22kube-state-metrics%22%2Cnamespace%3D%22p-ac-$app%22%2Cresource%3D%22cpu%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-prod-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)" -Lk | \
-  jq -r '.data.result[] | "\(.metric.pod) | \(.value[1] | tonumber * 100 | . * 100| floor / 100)"' | \
-   while IFS=" | " read -r pod cpu; do
-    cpu=${cpu:-0}
-    if [ "$printed_cpu" = false ]; then
-      echo "| Pod 名稱 | 一天 CPU 使用 (%) |"
-      echo "| --- | --- |"
-      printed_cpu=true
-    fi
-    echo "| $pod | $cpu(%) |"
-  done)
-  if [ -n "$cpu_output" ]; then
-    echo -e "$cpu_output"
-  else
-    echo "No valid data found for $app"
-  fi
-  echo ""
 
-  export memory_output=$(curl -s "http://prom.apps.k8s.104dc.com/api/v1/query?query=sum(container_memory_working_set_bytes%7Bnamespace%3D%22p-ac-$app%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-prod-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)%20%2F%20sum(kube_pod_container_resource_limits%7Bjob%3D%22kube-state-metrics%22%2Cnamespace%3D%22p-ac-$app%22%2Cresource%3D%22memory%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-prod-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)" -Lk | \
-  jq -r '.data.result[] | "\(.metric.pod) | \(.value[1] | tonumber * 100 | . * 100 | floor / 100)"' | \
-  while IFS=" | " read -r pod memory; do
-    if [ "$printed_memory" = false ]; then
-      echo "| Pod 名稱 | 一天記憶體使用 (%) |"
-      echo "| --- | --- |"
-      printed_memory=true
+  for reg in "${reg_list[@]}"; do
+    case "$reg" in
+      aws)
+        prometheus_url="https://k8s-prom-eks21.104dc.com"
+        cluster="eks21"
+        ;;
+      gcp)
+        prometheus_url="https://k8s-prom-gke21.104dc.com"
+        cluster="gke21"
+        ;;
+      *)
+        #prometheus_url="https://k8s-prom-prod.104dc.com"
+        prometheus_url="http://prom.apps.k8s.104dc.com"
+        cluster="prod"
+        ;;
+    esac
+
+    print_header=true
+    cpu_result=$(curl -s "$prometheus_url/api/v1/query?query=sum(node_namespace_pod_container%3Acontainer_cpu_usage_seconds_total%3Asum_irate%7Bnamespace%3D%22p-ac-$app%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-$cluster-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)%20%2F%20sum(kube_pod_container_resource_limits%7Bjob%3D%22kube-state-metrics%22%2Cnamespace%3D%22p-ac-$app%22%2Cresource%3D%22cpu%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-$cluster-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)" -Lk | \
+    jq -r '.data.result[] | "\(.metric.pod) | \(.value[1] | tonumber * 100 | . * 100| floor / 100)"' | \
+    while IFS=" | " read -r pod cpu; do
+      if [ "$print_header" = true ]; then
+        echo "| Pod 名稱 | 一天 CPU 使用 (%) |"
+        echo "| --- | --- |"
+        print_header=false
+      fi
+      cpu=${cpu:-0}
+      echo "| $pod | $cpu(%) |"
+    done)
+
+    echo ""
+    if [ -n "$cpu_output" ]; then
+      echo -e "$cpu_output"
+    else
+      echo "No valid data found for $app"
     fi
 
-    memory=${memory:-0}
-    # 打印表格行
-    echo "| $pod | $memory(%) |"
+    print_header=true
+    memory_result=$(curl -s "$prometheus_url/api/v1/query?query=sum(container_memory_working_set_bytes%7Bnamespace%3D%22p-ac-$app%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-$cluster-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)%20%2F%20sum(kube_pod_container_resource_limits%7Bjob%3D%22kube-state-metrics%22%2Cnamespace%3D%22p-ac-$app%22%2Cresource%3D%22memory%22%7D%20*%20on(namespace%2Cpod)%20group_left(workload%2Cworkload_type)%20namespace_workload_pod%3Akube_pod_owner%3Arelabel%7Bnamespace%3D%22p-ac-$app%22%2Cworkload%3D%22prod-$cluster-$app-web%22%2Cworkload_type%3D%22deployment%22%7D)%20by%20(pod)" -Lk | \
+    jq -r '.data.result[] | "\(.metric.pod) | \(.value[1] | tonumber * 100 | . * 100 | floor / 100)"' | \
+    while IFS=" | " read -r pod memory; do
+      if [ "$print_header" = true ]; then
+        echo "| Pod 名稱 | 一天記憶體使用 (%) |"
+        echo "| --- | --- |"
+        print_header=false
+      fi
+      memory=${memory:-0}
+      echo "| $pod | $memory(%) |"
+    done)
+
+    echo ""
+    if [ -n "$memory_output" ]; then
+      echo -e "$memory_output"
+    else
+      echo "No valid data found for $app"
+    fi
+
+    ingress_count=$(curl -s "$prometheus_url/api/v1/query?query=sum(increase(nginx_ingress_controller_requests%7Bcontroller_namespace%3D%22ingress-nginx%22%2Cexported_service%3D%22prod-$cluster-$app-web%22%7D%5B1d%5D))%20by%20(ingress)" -Lk | jq '.data.result[].value[1] | tonumber | floor')
+    ingress_result=$(echo "| Ingress 名稱 | 一天流量 |"
+      echo "| --- | --- |"
+      echo "| prod-$cluster-$app | $ingress_count |"
+    )
+
+    cpu_output+=$cpu_result
+    memory_output+=$memory_result
+    ingress_output+=$ingress_result
   done
-  )
-  if [ -n "$memory_output" ]; then
-    echo -e "$memory_output"
-  else
-    echo "No valid data found for $app"
-  fi
-  echo ""
-  printed_cpu=false
-  printed_memory=false
-  export count=$(curl -s "http://prom.apps.k8s.104dc.com/api/v1/query?query=sum(increase(nginx_ingress_controller_requests%7Bcontroller_namespace%3D%22ingress-nginx%22%2Cexported_service%3D%22prod-prod-$app-web%22%7D%5B1d%5D))%20by%20(ingress)" -Lk | jq '.data.result[].value[1] | tonumber | floor')
-  echo $count
+
+  export cpu_output
+  export memory_output
+  export ingress_output
+
   content=$(envsubst < note.md | jq -Rs .)
   curl -X POST "https://api.hackmd.io/v1/teams/104ContainerizationProject/notes" \
        -H "Authorization: Bearer 1EY2Y4U8SE637U1AFHCV3L9QHP6P7CDTHPKYNI6JRHJ1D7RA5B" \
--- ac/note.md
+++ ac/note.md
@@ -21,9 +21,11 @@ https://github.com/104corp/k8s-gitops-infra-rancher/blob/main/apps/config/overla
 
 ## 系統資源狀況
 以下是 30 天的數字。
-> [監控數據](https://grafana.apps.k8s.104dc.com/k8s/clusters/c-m-vpjqbm2z/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/a164a7f0339f99e89cea5cb47e9be617/kubernetes-compute-resources-workload?orgId=1&from=now-7d&to=now&var-datasource=Prometheus&var-cluster=&var-namespace=p-ac-$app&var-type=deployment&var-workload=prod-prod-$app-web)
+> [K8s-prod 監控數據](https://grafana.apps.k8s.104dc.com/k8s/clusters/c-m-vpjqbm2z/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/a164a7f0339f99e89cea5cb47e9be617/kubernetes-compute-resources-workload?orgId=1&from=now-7d&to=now&var-datasource=Prometheus&var-cluster=&var-namespace=p-ac-$app&var-type=deployment&var-workload=prod-prod-$app-web)
 
-> [監控數據](http://grafana.sys.104dc.com/d/ae79ll1qrwwlca/k8s-app-monthly-report?orgId=1&var-datasource=deOn58QVk&var-cluster=&var-namespace=$ns&var-type=deployment&var-ingress_vhost=$domain&from=now-7d&to=now)
+> [K8s-eks21 監控數據](https://k8s-grafana-eks21.104dc.com/k8s/clusters/c-n58cx/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/a164a7f0339f99e89cea5cb47e9be617/kubernetes-compute-resources-workload?orgId=1&from=now-7d&to=now&var-datasource=Prometheus&var-cluster=&var-namespace=p-ac-$app&var-type=deployment&var-workload=prod-eks21-$app-web)
+
+> [SE 監控數據](http://grafana.sys.104dc.com/d/ae79ll1qrwwlca/k8s-app-monthly-report?orgId=1&var-datasource=deOn58QVk&var-cluster=&var-namespace=$ns&var-type=deployment&var-ingress_vhost=$domain&from=now-7d&to=now)
 
 $cpu_output
 
@@ -32,10 +34,7 @@ $memory_output
 
 ### ingress 流量
 
-
-| 名稱 | 一天流量 |
-| --- | --- |
-| prod-prod-$app    |  $count   |
+$ingress_output
 
 ## 異常 Events
 
